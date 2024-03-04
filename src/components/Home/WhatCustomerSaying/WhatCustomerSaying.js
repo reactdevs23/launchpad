9cdc0d1d7a36652fb0a214cfebe1292f7d19e0c4
dalimkumar604 <dalimkumar212582@gmail.com>
2024-03-05 05:55:57 +0600

    iii

--- src/components/Home/WhatCustomerSaying/WhatCustomerSaying.js
+++ src/components/Home/WhatCustomerSaying/WhatCustomerSaying.js
@@ -5,7 +5,7 @@ import classes from "./WhatCustomerSaying.module.css";
 import SectionHeading from "../../common/SectionHeading/SectionHeading";
 import Highlight from "../../common/Highlight/Highlight";
 import Text from "../../common/Text/Text";
-import { avater } from "../../../asset";
+import { avater } from "../../../assets";
 
 import { GiRoundStar } from "react-icons/gi";
 import Slider from "react-slick";
